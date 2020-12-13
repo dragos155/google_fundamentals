76f682f9f98006510bb25357a6776cfa2e10c319
dragosmihaescu98 <58433700+dragos155@users.noreply.github.com>
2020-12-13 23:08:42 +0200

    Task2

--- app/src/main/java/com/example/google_fundamentals/MainActivity.java
+++ app/src/main/java/com/example/google_fundamentals/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.google_fundamentals;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText email_txt = findViewById(R.id.email);
+        EditText phone_txt = findViewById(R.id.phone);
+        CheckBox check= (CheckBox) findViewById(R.id.checkbox);
+        Button button_btn = findViewById(R.id.button);
+
+        button_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email=email_txt.getText().toString();
+                String phone=phone_txt.getText().toString();
+
+
+
+                if(TextUtils.isEmpty(email)){
+                    email_txt.setError("Enter your email!");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(phone)){
+                    phone_txt.setError("Password is Required.");
+                    return;
+                }
+
+                if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    email_txt.setError("Email is not correct");
+                    return;
+                }
+
+                if(!Patterns.PHONE.matcher(phone).matches()){
+                    phone_txt.setError("Phone is not correct");
+                    return;
+                }
+
+                if(!check.isChecked())
+                {check.setError("bifeaza");
+                    return;
+                }
+
+                if(Patterns.EMAIL_ADDRESS.matcher(email).matches() && Patterns.PHONE.matcher(phone).matches() && check.isChecked()){
+
+                    Toast.makeText(getApplicationContext(),email+"\n"+phone, Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
     }
 }
+
+
+
